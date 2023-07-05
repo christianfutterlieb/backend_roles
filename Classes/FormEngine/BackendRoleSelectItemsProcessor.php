b0aa79976c10cb2d613ed9e553f3c5519ba72686
Christian Futterlieb <christian@futterlieb.ch>
2023-07-05 15:01:46 +0200

    [TASK] Move roleIdentifier selector generation to an itemsProcFunc
    See: https://docs.typo3.org/c/typo3/cms-core/main/en-us/Changelog/9.5/Deprecation-86353-CacheManagerUsageInExt_localconfphp.html
    Cherry-picked from: c0c0c25f4ac089287dc7f9dcf4ba659dfafc7915

---
+++ Classes/FormEngine/BackendRoleSelectItemsProcessor.php
@@ -0,0 +1,66 @@
+<?php
+
+declare(strict_types=1);
+
+namespace AawTeam\BackendRoles\FormEngine;
+
+/*
+ * Copyright by Agentur am Wasser | Maeder & Partner AG
+ *
+ * For the full copyright and license information, please read the
+ * LICENSE file that was distributed with this source code.
+ *
+ * The TYPO3 project - inspiring people to share!
+ */
+
+use AawTeam\BackendRoles\Role\Definition\Formatter;
+use AawTeam\BackendRoles\Role\Definition\Loader;
+
+/**
+ * BackendRoleSelectItemsProcessor
+ */
+class BackendRoleSelectItemsProcessor
+{
+    /**
+     * @var Loader
+     */
+    private Loader $loader;
+
+    /**
+     * @var Formatter
+     */
+    private Formatter $formatter;
+
+    public function __construct(Loader $loader, Formatter $formatter)
+    {
+        $this->loader = $loader;
+        $this->formatter = $formatter;
+    }
+
+    public function process(array &$params): void
+    {
+        // Only add the items to be_groups.tx_backendroles_role_identifier
+        if ($params['table'] !== 'be_groups' || $params['field'] !== 'tx_backendroles_role_identifier') {
+            return;
+        }
+
+        $selectItems = [];
+        foreach ($this->loader->getRoleDefinitions() as $roleIdentifier => $roleDefinition) {
+            $selectItems[] = [
+                $this->formatter->formatTitle($roleDefinition),
+                $roleIdentifier,
+            ];
+        }
+
+        // Add items (if there are any)
+        if (!empty($selectItems)) {
+            // Create the empty item
+            if (!is_array($params['items'] ?? null)) {
+                $params['items'] = [
+                    ['', ''],
+                ];
+            }
+            $params['items'] = array_merge($params['items'], $selectItems);
+        }
+    }
+}
