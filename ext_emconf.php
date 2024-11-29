97264008cab8ece9512441610341bbd84f3e5dc7
Christian Futterlieb <christian@futterlieb.ch>
2024-11-29 12:34:52 +0100

    [BUGFIX] Correct php version dependency constraints

--- ext_emconf.php
+++ ext_emconf.php
@@ -20,7 +20,7 @@ $EM_CONF[$_EXTKEY] = [
     'version' => '3.1.1-dev',
     'constraints' => [
         'depends' => [
-            'php' => '8.1.0-8.2.99',
+            'php' => '8.1.0-8.3.99',
             'typo3' => '12.4.0-12.99.99',
         ],
         'conflicts' => [],
